d6415751185910b026c548904e6de0698f15f885
Fabian A <fabiansneider001@gmail.com>
2023-11-07 20:55:07 -0500

    Ultima implementacion

--- aplicacionparcial/lib/pages/ventana1.dart
+++ aplicacionparcial/lib/pages/ventana1.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:aplicacionparcial/pages/ventana2.dart';
-
 class HomeScreen extends StatelessWidget {
-  
   final List<IconData> iconList = [
     Icons.home,
     Icons.work,
@@ -12,79 +10,83 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-  IconData primericono = iconList[0];
-  IconData segundoicono = iconList[1];
-  IconData tercericono = iconList[2];
-  IconData cuartoicono = iconList[3];
+    IconData primericono = iconList[0];
+    IconData segundoicono = iconList[1];
+    IconData tercericono = iconList[2];
+    IconData cuartoicono = iconList[3];
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
         leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-            },
-          ),
-        title:  const Text('AppParcial'),
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: const Text('AppParcial'),
         actions: [
           IconButton(
-            icon:  const Icon(Icons.account_circle),
-            onPressed: () {
-            },
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {},
           ),
           IconButton(
-            icon:  const Icon(Icons.shopping_cart),
-            onPressed: () {
-            },
-          ),          
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
         ],
       ),
       body: Center(
-          child: Column (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            Row (
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          const Text('Segundo parcial',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Card(
-                child: Icon(primericono, size: 28,),
-                color: const Color.fromARGB(255, 226, 227, 227),
+                Container(
+                  width: 100, 
+                  height: 100, 
+                  child: Card(
+                    child: Icon(primericono, size: 28),
+                    color: Color.fromARGB(255, 145, 228, 145)),
                 ),
-                Card(
-                color: const Color.fromARGB(255, 226, 227, 227),
-                child: Icon(segundoicono, size: 28),
+                Container(
+                  width: 100, 
+                  height: 100, 
+                  child: Card(
+                    color: const Color.fromARGB(255, 145, 228, 145),
+                    child: Icon(segundoicono, size: 28),
+                  ),
                 ),
               ],
             ),
-            Row (
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Card(
-                child: Icon(tercericono, size: 28,),
-                color: const Color.fromARGB(255, 226, 227, 227),
+                Container(
+                  width: 100, 
+                  height: 100, 
+                  child: Card(
+                    child: Icon(tercericono, size: 28),
+                    color: const Color.fromARGB(255, 145, 228, 145),
+                  ),
                 ),
-                Card(
-                color: const Color.fromARGB(255, 226, 227, 227),
-                child: Icon(cuartoicono, size: 28),
+                Container(
+                  width: 100, 
+                  height: 100, 
+                  child: Card(
+                    color: const Color.fromARGB(255, 145, 228, 145),
+                    child: Icon(cuartoicono, size: 28),
+                  ),
                 ),
               ],
             ),
-            ],
-          ),
+          ],
         ),
-      /*body: Center(
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) {
-            final cardTitle = 'Tarjeta ${index + 1}';
-            final cardIcon = iconMap[cardTitle];
-
-            return CardWidget(cardTitle: cardTitle, cardIcon: cardIcon);
-          },
-        ),
-      ),*/
+      ),
     );
   }
 }
--- aplicacionparcial/lib/pages/ventana2.dart
+++ aplicacionparcial/lib/pages/ventana2.dart
@@ -1,22 +1,67 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-class CardWidget extends StatelessWidget {
-  final String cardTitle;
-  final IconData? cardIcon;
-
-  CardWidget({required this.cardTitle, required this cardIcon});
-
+class HomeScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(cardIcon, size: 64.0),
-          SizedBox(height: 8.0),
-          Text(cardTitle, style: TextStyle(fontSize: 16.0)),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: const Text('AppParcial'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('larutaetcetcetc.jpg'), 
+            ),
+            SizedBox(height: 20),
+            FloatingActionButton(
+              onPressed: () {
+              showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Popup',
+                  style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+              ),
+            ),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(
+                  child: Text('.....................'),
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cerrar')),
+            ],
+          );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
-}*/
+}
